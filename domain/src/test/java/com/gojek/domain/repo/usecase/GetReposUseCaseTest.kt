ef58b76a20d59236111c7dde7473ff068f38111a
chapp <chapp.hkg@gmail.com>
2020-03-22 01:52:57 +0800

    test: add unit test for GetReposUseCase

---
+++ domain/src/test/java/com/gojek/domain/repo/usecase/GetReposUseCaseTest.kt
@@ -0,0 +1,45 @@
+package com.gojek.domain.repo.usecase
+
+import com.gojek.domain.repo.IRepoRepository
+import com.gojek.domain.repo.entity.RepoEntity
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import io.reactivex.Single
+import io.reactivex.schedulers.TestScheduler
+import org.junit.Before
+import org.junit.Test
+
+class GetReposUseCaseTest {
+
+    @MockK
+    private lateinit var mainScheduler: TestScheduler
+    @MockK
+    private lateinit var ioScheduler: TestScheduler
+    @MockK
+    private lateinit var repoRepository: IRepoRepository
+    @MockK
+    private lateinit var single: Single<List<RepoEntity>>
+
+    private lateinit var getReposUseCase: GetReposUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        getReposUseCase = GetReposUseCase(mainScheduler, ioScheduler, repoRepository)
+    }
+
+    @Test
+    fun execute() {
+        every { repoRepository.getRepos() } returns single
+        every { single.subscribeOn(any()) } returns single
+        every { single.observeOn(any()) } returns single
+
+        getReposUseCase.execute()
+
+        verify { repoRepository.getRepos() }
+        verify { single.subscribeOn(eq(ioScheduler)) }
+        verify { single.observeOn(eq(mainScheduler)) }
+    }
+}
