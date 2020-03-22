c11f967ea2406e1fc8f13c0c8a12dbfeff3c2e38
chapp <chapp.hkg@gmail.com>
2020-03-22 16:12:28 +0800

    feat: initialize Fresco

--- app/src/main/java/com/gojek/assignment/app/App.kt
+++ app/src/main/java/com/gojek/assignment/app/App.kt
@@ -1,12 +1,28 @@
 package com.gojek.assignment.app
 
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.imagepipeline.core.ImagePipelineConfig
 import com.gojek.assignment.arch.dependency.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+
 
 class App : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.factory().create(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        initSdk()
+    }
+
+    private fun initSdk() {
+        val config = ImagePipelineConfig.newBuilder(this)
+            .setDownsampleEnabled(true) // enable downsample to lower memory usage
+            .build()
+
+        Fresco.initialize(this, config)
     }
 }
