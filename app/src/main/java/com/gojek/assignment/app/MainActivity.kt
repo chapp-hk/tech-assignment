1cc0b0bc70b02e73bfa861b95215da6f94b4984b
chapp <chapp.hkg@gmail.com>
2020-03-22 10:27:05 +0800

    feat: add main activity

---
+++ app/src/main/java/com/gojek/assignment/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.gojek.assignment.app
+
+import androidx.appcompat.app.AppCompatActivity
+import com.gojek.assignment.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+}
