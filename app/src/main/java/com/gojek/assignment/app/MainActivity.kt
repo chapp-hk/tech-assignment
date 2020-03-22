10ab573aa7b7370ac9801207a0c98ff5a16865c5
chapp <chapp.hkg@gmail.com>
2020-03-22 16:52:05 +0800

    style: remove redundant brackets

--- app/src/main/java/com/gojek/assignment/app/MainActivity.kt
+++ app/src/main/java/com/gojek/assignment/app/MainActivity.kt
@@ -3,5 +3,4 @@ package com.gojek.assignment.app
 import androidx.appcompat.app.AppCompatActivity
 import com.gojek.assignment.R
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
