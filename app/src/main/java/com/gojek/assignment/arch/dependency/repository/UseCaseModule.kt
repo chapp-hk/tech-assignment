9dd97202963375bab747a316d43547fc937f7ec7
chapp <chapp.hkg@gmail.com>
2020-03-22 13:12:16 +0800

    feat: add use case module for dagger

---
+++ app/src/main/java/com/gojek/assignment/arch/dependency/repository/UseCaseModule.kt
@@ -0,0 +1,22 @@
+package com.gojek.assignment.arch.dependency.repository
+
+import com.gojek.domain.repo.IRepoRepository
+import com.gojek.domain.repo.usecase.GetReposUseCase
+import dagger.Module
+import dagger.Provides
+import io.reactivex.Scheduler
+import io.reactivex.annotations.SchedulerSupport
+import javax.inject.Named
+
+@Module(includes = [RepositoryModule::class])
+class UseCaseModule {
+
+    @Provides
+    internal fun provideGetReposUseCase(
+        @Named(SchedulerSupport.CUSTOM) mainScheduler: Scheduler,
+        @Named(SchedulerSupport.IO) ioScheduler: Scheduler,
+        repoRepository: IRepoRepository
+    ): GetReposUseCase {
+        return GetReposUseCase(mainScheduler, ioScheduler, repoRepository)
+    }
+}
