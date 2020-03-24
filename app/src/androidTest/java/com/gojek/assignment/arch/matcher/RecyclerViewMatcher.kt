405e06adfba1c6584cd494e422db2d2b7c28e9a2
chapp <chapp.hkg@gmail.com>
2020-03-25 04:52:36 +0800

    refactor: delete unuse code

--- app/src/androidTest/java/com/gojek/assignment/arch/matcher/RecyclerViewMatcher.kt
+++ app/src/androidTest/java/com/gojek/assignment/arch/matcher/RecyclerViewMatcher.kt
@@ -7,10 +7,8 @@ import androidx.test.espresso.matcher.BoundedMatcher
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 
-
 object RecyclerViewMatcher {
     fun atPosition(position: Int, itemMatcher: Matcher<View>): Matcher<View> {
-        checkNotNull(itemMatcher)
         return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
             override fun describeTo(description: Description) {
                 description.appendText("has item at position $position: ")
