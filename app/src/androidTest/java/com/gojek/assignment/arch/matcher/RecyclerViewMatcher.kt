61562d4152b055f3a80beb37f69f91cd2b4dead4
chapp <chapp.hkg@gmail.com>
2020-03-26 01:15:58 +0800

    style: add line space

--- app/src/androidTest/java/com/gojek/assignment/arch/matcher/RecyclerViewMatcher.kt
+++ app/src/androidTest/java/com/gojek/assignment/arch/matcher/RecyclerViewMatcher.kt
@@ -8,8 +8,10 @@ import org.hamcrest.Description
 import org.hamcrest.Matcher
 
 object RecyclerViewMatcher {
+
     fun atPosition(position: Int, itemMatcher: Matcher<View>): Matcher<View> {
         return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+
             override fun describeTo(description: Description) {
                 description.appendText("has item at position $position: ")
                 itemMatcher.describeTo(description)
