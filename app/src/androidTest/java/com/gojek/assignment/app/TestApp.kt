ac2614a33c4796eef507f9ab4de9156997b5c6c4
chapp <chapp.hkg@gmail.com>
2020-03-26 11:14:13 +0800

    test: add okhttp idling resource

--- app/src/androidTest/java/com/gojek/assignment/app/TestApp.kt
+++ app/src/androidTest/java/com/gojek/assignment/app/TestApp.kt
@@ -1,3 +1,19 @@
 package com.gojek.assignment.app
 
-class TestApp : App()
+import com.gojek.assignment.arch.dependency.DaggerTestAppComponent
+import com.gojek.assignment.arch.dependency.TestAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class TestApp : App() {
+
+    lateinit var testAppComponent: TestAppComponent
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        if (!::testAppComponent.isInitialized) {
+            testAppComponent = DaggerTestAppComponent.factory().create(this)
+        }
+
+        return testAppComponent
+    }
+}
---
+++ app/src/androidTest/java/com/gojek/assignment/arch/OkHttpIdlingResource.kt
@@ -0,0 +1,27 @@
+package com.gojek.assignment.arch
+
+import androidx.test.espresso.IdlingResource
+import androidx.test.espresso.IdlingResource.ResourceCallback
+import okhttp3.OkHttpClient
+import javax.inject.Inject
+
+class OkHttpIdlingResource
+@Inject constructor(
+    private val name: String,
+    private val okHttpClient: OkHttpClient
+) : IdlingResource {
+
+    override fun getName(): String {
+        return name
+    }
+
+    override fun isIdleNow(): Boolean {
+        return okHttpClient.dispatcher().runningCallsCount() == 0
+    }
+
+    override fun registerIdleTransitionCallback(callback: ResourceCallback) {
+        okHttpClient.dispatcher().setIdleCallback {
+            callback.onTransitionToIdle()
+        }
+    }
+}
